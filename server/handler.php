b4ae5e5d31265ed148de18a0d15f73f275d277df
molin <wudaxing0829@126.com>
2018-05-10 16:59:55 +0800

    引入db文件 Signed-off-by: molin <wudaxing0829@126.com>

--- server/handler.php
+++ server/handler.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '../conn/conn.php';
 /**
  * Do not use or reference this directly from your client-side code.
  * Instead, this should be required via the endpoint.php or endpoint-cors.php
